9be6c2b1571be9d3fad60bd29fa52036eb9454ad
Luca Nguyen <108933241+smoluca123@users.noreply.github.com>
2024-07-10 20:44:48 +0700

    add hooks useDebounceFn

--- src/hooks/useDebounceFn.ts
+++ src/hooks/useDebounceFn.ts
@@ -2,6 +2,16 @@ import { useEffect } from 'react';
 import { DEFAULT_DELAY } from '../constant';
 import { useTimeout } from './useTimeout';
 
+/**
+ *
+ * @param fn The function to debounce.
+ * @param delay The delay in milliseconds. Default is 300ms.
+ * @param deps The dependencies array.
+ * @example
+ * const debouncedFn = useDebounceFn(() => console.log('Debounced'),500, [someValue]  // Depends on someValue to trigger the debouncing.)
+ *
+ */
+
 export function useDebounceFn<T>(
   fn: (...args: any[]) => T,
   delay: number = DEFAULT_DELAY,
